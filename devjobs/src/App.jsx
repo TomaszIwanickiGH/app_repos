323204812bd70b438977419a59861ccba1ca4f1d
Tomasz <tomasz.iwanicki@onet.pl>
2023-09-27 10:25:21 +0200

    .

--- devjobs/src/App.jsx
+++ devjobs/src/App.jsx
@@ -3,12 +3,16 @@ import Home from './components/Home';
 import JobDetails from './components/JobDetails';
 
 import { Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div>
       <Navbar />
-      <div className="relative">
+      <div className="relative border-[20px] border-lime-500">
         <div className="absolute xl:-top-12 lg:-top-4 -top-12 w-full lg:px-40 md:px-20 px-8">
           <Routes>
             <Route
@@ -22,6 +26,7 @@ const App = () => {
           </Routes>
         </div>
       </div>
+      {/* {location.pathname.includes('/details') && <div className="">sdsdddddddd</div>} */}
     </div>
   );
 };
--- devjobs/src/components/JobDetails.jsx
+++ devjobs/src/components/JobDetails.jsx
@@ -1,9 +1,99 @@
 import { useParams } from 'react-router-dom';
+import Button from './Button';
+
+import { data } from '../constants';
 
 const JobDetails = () => {
   const { id } = useParams();
+  const chosenJob = data.filter((job) => job.id === parseInt(id));
 
-  return <div className="flex flex-col gap-20 lg:w-1/2 w-full mx-auto">JobDetails {id}</div>;
+  return (
+    <>
+      <div className="flex flex-col gap-12 lg:w-3/5 w-full mx-auto mb-20">
+        {chosenJob.map((job) => (
+          <div
+            className="flex rounded-sm bg-white w-full"
+            key={job.id}
+          >
+            <div
+              style={{ backgroundColor: job.logoBackground }}
+              className="flex justify-center items-center w-[140px] px-4"
+            >
+              <img
+                className="w-full"
+                src={job.logo}
+                alt=""
+              />
+            </div>
+            <div className="flex justify-between items-center w-full p-6">
+              <div className="flex flex-col gap-4">
+                <h3 className="text-[20px] font-bold">{job.company}</h3>
+                <p className="text-[15px] text-neutral-500">{job.company.toLowerCase()}.com</p>
+              </div>
+              <Button
+                label="Company Site"
+                light
+              />
+            </div>
+          </div>
+        ))}
+        {chosenJob.map((job) => (
+          <div
+            className="flex flex-col bg-white rounded-lg gap-8 p-8"
+            key={job.id}
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex flex-1 flex-col gap-2">
+                <div className="flex items-center gap-2 text-neutral-500 text-[14px]">
+                  <p>{job.postedAt}</p>
+                  <p>•</p>
+                  <p>{job.contract}</p>
+                </div>
+                <h2 className="font-bold text-[28px]">{job.position}</h2>
+                <p className="text-violet font-bold text-[15px]">{job.location}</p>
+              </div>
+              <Button label="Apply Now" />
+            </div>
+
+            <p className="leading-6 text-neutral-500">{job.description}</p>
+
+            <div className="flex flex-col gap-6">
+              <h3 className="font-bold text-[20px]">Requirements</h3>
+              <p className="leading-6 text-neutral-500">{job.requirements.content}</p>
+              <div className="flex flex-col gap-4">
+                {job.requirements.items.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex gap-6 justify-start items-center"
+                  >
+                    <p className="text-[30px] text-violet">•</p>
+                    <p className="text-neutral-500">{item}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-6">
+              <h3 className="font-bold text-[20px]">What You Will do</h3>
+              <p className="leading-6 text-neutral-500">{job.role.content}</p>
+              <div className="flex flex-col gap-4">
+                {job.role.items.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex gap-6 justify-start"
+                  >
+                    <p className="text-[16px] font-bold text-violet">{index + 1}</p>
+                    <p className="text-neutral-500">{item}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="">sdddd</div>
+    </>
+  );
 };
 
 export default JobDetails;
